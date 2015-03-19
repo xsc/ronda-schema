0904e49d850e75806080702c44b6d2f504dc5259
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-03-19 21:32:45 +0100

    upgrade dependencies.

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
-                 [org.clojure/tools.reader "0.8.13"]
-                 [prismatic/schema "0.3.7"]
-                 [potemkin "0.3.11"]]
+                 [org.clojure/tools.reader "0.8.16"]
+                 [prismatic/schema "0.4.0"]
+                 [potemkin "0.3.12"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]
                                   [joda-time "2.7"]]
                    :plugins [[lein-midje "3.1.3"]
